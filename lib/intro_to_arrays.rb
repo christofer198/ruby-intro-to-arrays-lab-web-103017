7ccfa6b2ed033c262594df91bca96c0ab3314fcf
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-13 16:32:36 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,15 @@ end
 def array_with_two_elements
   array = ["apple", "google"]
 end
+
+def first_element(array)
+  return array[1]
+end
+
+def third_element(array)
+  return array[3]
+end
+
+def last_element(array)
+  return array[-1]
+end
