7d32a476eee2d333d48a6657a7e8cdb540b6b137
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-13 16:35:20 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,11 +8,11 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  return array[1]
+  return array[0]
 end
 
 def third_element(array)
-  return array[3]
+  return array[2]
 end
 
 def last_element(array)
